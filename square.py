18fdaa780c05419d43222d7f52b04063fd754d11
jayanthkumarmadduri-code <jayanthkumarmadduri@gmail.com>
2026-03-08 08:21:54 +0000

    hfjyfj

---
+++ square.py
@@ -0,0 +1,8 @@
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(1, rows + 1):  
+    for j in range(1, rows + 1):  
+        if i == 1 or i == rows or j == 1 or j == rows :  
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")  # Print space inside
+    print()
