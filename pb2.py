18d254c1fdb901dc74a06bb43997229b04aae58d
jayanthkumarmadduri-code <jayanthkumarmadduri@gmail.com>
2026-05-22 09:19:46 +0000

    bbcsmk

---
+++ pb2.py
@@ -0,0 +1,12 @@
+n1 = float(input("Enter 1st number : "))
+n2 = float(input("Enter 2nd number : "))
+
+sum = n1 + n2
+difference = n1 - n2
+product = n1 * n2
+quotient = n1 / n2
+
+print("Sum =", sum)
+print("Difference =", difference)
+print("Product =", product)
+print("Quotient =", quotient)
