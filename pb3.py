8680bca3b6f52111d0475fb3a14c90605544f56d
jayanthkumarmadduri-code <jayanthkumarmadduri@gmail.com>
2026-05-22 09:22:17 +0000

    gjcksvcjs

---
+++ pb3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
+num3 = float(input("Enter a float number: "))
+num1 = float(num1)
+num2 = float(num2)
+average = (num1 + num2 + num3) / 3
+print("Average =", average)
