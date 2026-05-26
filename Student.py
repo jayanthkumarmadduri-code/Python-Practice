ab1eb9abc512c454723fab85af920fd4d22b930d
jayanthkumarmadduri-code <jayanthkumarmadduri@gmail.com>
2026-05-26 08:44:44 +0000

    constructors

---
+++ Student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, cgpa):
+        self.name = name
+        self.cgpa = cgpa
+
+stu1 = Student("Rahul", 9.2)
+stu2 = Student("Sidhi", 8.9)
+stu3 = Student("Ronak", 8.6) 
+
+print(stu1.cgpa)
+print(stu2.name)
+print(stu3.name)
+
+        
